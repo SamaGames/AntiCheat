33fb730a3188124ee38421e2d094a37af9e37ab9
Silvanosky <geekpower14@gmail.com>
2015-12-23 12:38:10 +0100

    test new thing

---
+++ src/main/java/net/samagames/samaritan/cheats/antixray/listeners/OrebfuscatorPlayerListener.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2011-2014 lishid.  All rights reserved.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation,  version 3.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.samagames.samaritan.cheats.antixray.listeners;
+
+
+import net.samagames.samaritan.cheats.antixray.OrebfuscatorConfig;
+import net.samagames.samaritan.cheats.antixray.hithack.BlockHitManager;
+import net.samagames.samaritan.cheats.antixray.obfuscation.BlockUpdate;
+import net.samagames.samaritan.cheats.antixray.obfuscation.ProximityHider;
+import org.bukkit.Material;
+import org.bukkit.event.Event.Result;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class OrebfuscatorPlayerListener implements Listener {
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        BlockHitManager.clearHistory(event.getPlayer());
+        if (OrebfuscatorConfig.UseProximityHider) {
+            ProximityHider.clearPlayer(event.getPlayer());
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK || event.useInteractedBlock() == Result.DENY)
+            return;
+
+        //For using a hoe for farming
+        if (event.getItem() != null &&
+                event.getItem().getType() != null &&
+                (event.getMaterial() == Material.DIRT || event.getMaterial() == Material.GRASS) &&
+                ((event.getItem().getType() == Material.WOOD_HOE) ||
+                        (event.getItem().getType() == Material.IRON_HOE) ||
+                        (event.getItem().getType() == Material.GOLD_HOE) ||
+                        (event.getItem().getType() == Material.DIAMOND_HOE))) {
+            BlockUpdate.Update(event.getClickedBlock());
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerChangeWorld(PlayerChangedWorldEvent event) {
+        BlockHitManager.clearHistory(event.getPlayer());
+        if (OrebfuscatorConfig.UseProximityHider) {
+            ProximityHider.clearPlayer(event.getPlayer());
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerMove(PlayerMoveEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
+
+        if (OrebfuscatorConfig.UseProximityHider) {
+            ProximityHider.playerMoved(event.getPlayer(), event.getFrom());
+        }
+    }
+}
---
+++ src/main/java/net/samagames/samaritan/cheats/antixray/obfuscation/BlockUpdate.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2011-2014 lishid.  All rights reserved.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation,  version 3.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.samagames.samaritan.cheats.antixray.obfuscation;
+
+import net.samagames.samaritan.cheats.antixray.OrebfuscatorConfig;
+import net.samagames.samaritan.cheats.antixray.internal.MinecraftInternals;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BlockUpdate {
+    public static boolean needsUpdate(Block block) {
+        return !OrebfuscatorConfig.isBlockTransparent(block.getTypeId());
+    }
+
+    public static void Update(Block block) {
+        if (!needsUpdate(block)) {
+            return;
+        }
+
+        Update(Arrays.asList(new Block[]{block}));
+    }
+
+    public static void Update(List<Block> blocks) {
+        if (blocks.isEmpty()) {
+            return;
+        }
+
+        Set<Block> updateBlocks = new HashSet<Block>();
+        for (Block block : blocks) {
+            if (needsUpdate(block)) {
+                updateBlocks.addAll(GetAjacentBlocks(new HashSet<Block>(), block, OrebfuscatorConfig.UpdateRadius));
+            }
+        }
+
+        World world = blocks.get(0).getWorld();
+
+        sendUpdates(world, updateBlocks);
+    }
+
+    private static void sendUpdates(World world, Set<Block> blocks) {
+        for (Block block : blocks) {
+            MinecraftInternals.notifyBlockChange(world, block.getX(), block.getY(), block.getZ());
+        }
+    }
+
+    public static HashSet<Block> GetAjacentBlocks(HashSet<Block> allBlocks, Block block, int countdown) {
+        if (block == null) {
+            return allBlocks;
+        }
+
+        if ((OrebfuscatorConfig.isObfuscated(block.getTypeId(), block.getWorld().getEnvironment())
+                || OrebfuscatorConfig.isDarknessObfuscated(block.getTypeId()))) {
+            allBlocks.add(block);
+        }
+
+        if (countdown > 0) {
+            countdown--;
+            World world = block.getWorld();
+            GetAjacentBlocks(allBlocks, CalculationsUtil.getBlockAt(world, block.getX() + 1, block.getY(), block.getZ()), countdown);
+            GetAjacentBlocks(allBlocks, CalculationsUtil.getBlockAt(world, block.getX() - 1, block.getY(), block.getZ()), countdown);
+            GetAjacentBlocks(allBlocks, CalculationsUtil.getBlockAt(world, block.getX(), block.getY() + 1, block.getZ()), countdown);
+            GetAjacentBlocks(allBlocks, CalculationsUtil.getBlockAt(world, block.getX(), block.getY() - 1, block.getZ()), countdown);
+            GetAjacentBlocks(allBlocks, CalculationsUtil.getBlockAt(world, block.getX(), block.getY(), block.getZ() + 1), countdown);
+            GetAjacentBlocks(allBlocks, CalculationsUtil.getBlockAt(world, block.getX(), block.getY(), block.getZ() - 1), countdown);
+        }
+
+        return allBlocks;
+    }
+}
