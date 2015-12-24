bf6525aa64040c6898753701950431cd1f5d0545
Silvanosky <geekpower14@gmail.com>
2015-12-24 13:59:14 +0100

    Finally works

---
+++ src/main/java/net/samagames/samaritan/cheats/antixray/hook/OrebfuscatorPlayerHook.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2011-2014 lishid.  All rights reserved.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation,  version 3.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.samagames.samaritan.cheats.antixray.hook;
+
+import net.samagames.samaritan.cheats.antixray.internal.PlayerHook;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class OrebfuscatorPlayerHook implements Listener {
+    private static PlayerHook playerHook;
+
+    private static PlayerHook getPlayerHook() {
+        if (playerHook == null) {
+            playerHook = new PlayerHook();
+        }
+
+        return playerHook;
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onPlayerJoin(final PlayerJoinEvent event) {
+        PlayerHook playerHook = getPlayerHook();
+        playerHook.HookChunkQueue(event.getPlayer());
+    }
+}
---
+++ src/main/java/net/samagames/samaritan/cheats/antixray/listeners/OrebfuscatorEntityListener.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2011-2014 lishid.  All rights reserved.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation,  version 3.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.samagames.samaritan.cheats.antixray.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.*;
+
+import net.samagames.samaritan.cheats.antixray.obfuscation.BlockUpdate;
+
+public class OrebfuscatorEntityListener implements Listener {
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onEntityExplode(EntityExplodeEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
+
+        BlockUpdate.Update(event.blockList());
+    }
+}
